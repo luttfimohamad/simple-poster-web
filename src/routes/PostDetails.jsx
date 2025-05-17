de865bf303d94d7d55aca5b4c359a7fb4bb4bd6a
luttfimohamad <luttfimohamad@gmail.com>
2025-05-17 17:54:56 +0700

    Add delete functionality and update styles in PostDetails component

--- src/routes/PostDetails.jsx
+++ src/routes/PostDetails.jsx
@@ -1,10 +1,36 @@
-import { useLoaderData, Link } from 'react-router-dom';
-
+import {
+  useLoaderData,
+  useRevalidator,
+  Link,
+  useNavigate,
+} from 'react-router-dom';
 import Modal from '../components/Modal';
 import classes from './PostDetails.module.css';
 
 export default function PostDetails() {
   const post = useLoaderData();
+  const revalidator = useRevalidator();
+  const navigate = useNavigate(); // <- tambahkan ini
+
+  async function handleDelete() {
+    const shouldDelete = window.confirm(
+      'Are you sure you want to delete this post?'
+    );
+    if (!shouldDelete) return;
+
+    const response = await fetch('http://localhost:8080/posts/' + post.id, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      navigate('/'); // navigasi ke halaman utama
+      setTimeout(() => {
+        revalidator.revalidate(); // force refresh list post
+      }, 100);
+    } else {
+      alert('Failed to delete the post.');
+    }
+  }
 
   if (!post) {
     return (
@@ -21,17 +47,26 @@ export default function PostDetails() {
       </Modal>
     );
   }
+
   return (
     <Modal>
       <main className={classes.details}>
         <p className={classes.author}>{post.author}</p>
         <p className={classes.text}>{post.body}</p>
+        <div className={classes.actions}>
+          <button onClick={handleDelete}>Delete Post</button>
+          <button>
+            <Link to=".." type="button">
+              Back
+            </Link>
+          </button>
+        </div>
       </main>
     </Modal>
   );
 }
 
-export async function loader({params}) {
+export async function loader({ params }) {
   const response = await fetch('http://localhost:8080/posts/' + params.id);
   const resData = await response.json();
   return resData.post;
